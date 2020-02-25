9e2bffd6bf548d556784c42fd27693b9d81f609e
Pablo Marcano <pablo1012@outlook.com>
2020-02-25 10:25:13 -0300

    Fix Codesandbox Script and change base color

--- src/index.js
+++ src/index.js
@@ -4,4 +4,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
+
+// Try changing the $background color  variable on Styles.scss file!
 ReactDOM.render(<App />, rootElement);
